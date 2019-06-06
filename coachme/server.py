b4540bd0325a0985faecb60e00634b48de63b7c6
theorist <theorist17@gmail.com>
2019-06-07 00:55:51 +0900

    server handling device

--- coachme/server.py
+++ coachme/server.py
@@ -1,5 +1,25 @@
 import socket 
 import json
+from _thread import *
+import threading
+
+print_lock = threading.Lock()
+
+def device(c):
+    while True:
+        jsondata = c.recv(1024).decode()
+        if not jsondata:
+            print_lock.release()
+            break
+        print("Device : "+jsondata)
+        jsonout = {}
+        #jsonout['img'] = data.encode('base64')
+        jsonout['test'] = "json is working.."
+        jsondump = json.dumps(jsonout)
+        c.send(jsondump.encode())
+    c.close()
+    print("Closed.")
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #host = gethostbyname(gethostname())
 port = 17171
@@ -11,16 +31,9 @@ print("Listenning...")
 while True:
     c, addr = s.accept()
     try:
+        print_lock.acquire()
         print('Got connection from'+str(addr))
-        message = c.recv(1024).decode()
-        print("Client : "+message)
-        jsonout = {}
-        #jsonout['img'] = data.encode('base64')
-        jsonout['test'] = "json is working.."
-        jsondump = json.dumps(jsonout)
-        c.send(jsondump.encode())
-        c.close()
-        print('Closed.')
+        start_new_thread(device, (c,))
     except IOError:
         c.send('IOError!')
         c.close()
