c1fabfe7e2b7b81d6e5d1ab769a03f4ba5df72e7
theorist <theorist17@gmail.com>
2019-06-08 08:45:53 +0900

    device and client

--- coachme/device.py
+++ coachme/device.py
@@ -1,9 +1,22 @@
-from socket import *
+import socket
+import json
+
+#conn
 serverName = '175.113.152.102'
 serverPort = 17171
-
 print("Connecting :", serverName, serverPort)
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
-clientSocket.sendall('Hello'.encode())
-print("Server : "+clientSocket.recv(1024).decode())
+device = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+device.connect((serverName, serverPort))
+
+#send
+data = {}
+i = 1
+while True:
+    data['img'] = i
+    i += 1
+    i %= 10000
+    jsondata = json.dumps(data) 
+    print(jsondata)
+    device.send(('%d\n' % len(jsondata)).encode())
+    device.sendall(jsondata.encode())
+
