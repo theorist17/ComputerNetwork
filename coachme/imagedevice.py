cee249fb3c52b0fa92ccf741ffec8dc9791304fa
theorist <theorist17@gmail.com>
2019-06-09 19:54:42 +0900

    add imagedevice.py sending an image for end-to-end com test

---
+++ coachme/imagedevice.py
@@ -0,0 +1,18 @@
+import socket
+import json
+
+#conn
+serverName = '175.113.152.102'
+serverPort = 17171
+print("Connecting :", serverName, serverPort)
+device = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+device.connect((serverName, serverPort))
+
+#send
+data = {}
+i = 1
+data['img'] = i
+jsondata = json.dumps(data) 
+print(jsondata)
+device.send(('%d\n' % len(jsondata)).encode())
+device.sendall(jsondata.encode())
